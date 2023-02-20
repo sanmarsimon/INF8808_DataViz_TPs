3370b4a08c8f0e684ab02fba6876e0a6e3f45314
Sanmar Simon <sanmarsimon@gmail.com>
2023-02-20 01:01:38 -0500

    Heatmap hover template

--- TP3/src/hover_template.py
+++ TP3/src/hover_template.py
@@ -16,6 +16,14 @@ def get_heatmap_hover_template():
     '''
     # TODO : Define and return the hover template
 
+    # Use the font 'Roboto Slab' and bold for the labels and the font 'Roboto' and regular weight for the values
+    return "<br>".join([
+        "<span style='font-family: Roboto Slab; font-weight: bold'>Neighborhood</span> : %{y}",
+        "<span style='font-family: Roboto Slab; font-weight: bold'>Year</span> : %{x}",
+        "<span style='font-family: Roboto; font-weight: bold'>Trees</span> : %{z}"
+    ])
+
+
 def get_linechart_hover_template():
     '''
         Sets the template for the hover tooltips in the heatmap.
